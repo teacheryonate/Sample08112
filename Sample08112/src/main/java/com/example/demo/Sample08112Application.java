abbda3812a38d854f4bfafb5611f2d47791624ae
teacheryonate <teacheryo@nate.com>
2023-08-11 11:30:19 +0900

    first work

---
+++ Sample08112/src/main/java/com/example/demo/Sample08112Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample08112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample08112Application.class, args);
+	}
+
+}
